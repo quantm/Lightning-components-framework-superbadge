1c0ba5cb928f5832fcb4d3324155da0b6c8b7687
Anil <anil.mohan@silverlinecrm.com>
2018-08-30 11:48:49 +0530

    lightning components framework specialist source code
    lightning components framework specialist source code.

---
+++ src/aura/BoatSearch/BoatSearchController.js
@@ -0,0 +1,27 @@
+({
+    onFormSubmit: function(component, event, helper){
+        console.log("event received by BoatSearchController.js");
+        var formData = event.getParam("formData");
+        var boatTypeId = formData.boatTypeId;
+        debugger
+        console.log("boatTypeId extracted: "+boatTypeId);
+        
+        
+        var BSRcmp = component.find("BSRcmp");
+        var auraMethodResult = BSRcmp.search(boatTypeId);
+        console.log("auraMethodResult: " + auraMethodResult);
+        //helper.onSearch(component,event);
+    },
+    doSearch:function(component, event, helper) {
+        
+    },
+    search: function(component, event, helper){
+        console.log("BSRController: search called");
+        var params = event.getParam('arguments');
+        console.log("boatTypeId extracted: " + params.boatTypeId);
+        component.set("v.boatTypeId", params.boatTypeId);
+        helper.onSearch(component,event);
+        return "search complete.";
+    }
+    
+})
